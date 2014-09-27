e3d367fe66d7edf5bc84d158385c0776a200f7cf
Bujar <btagani@cmu.edu>
2014-09-27 16:33:54 -0400

    home page updated

--- RoadTripPlanner/urls.py
+++ RoadTripPlanner/urls.py
@@ -10,10 +10,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'foodictd.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 	url(r'^$', TemplateView.as_view(template_name="index.html")),
-
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^$', TemplateView.as_view(template_name="index.html")),
     # url(r'^$', 'restaurants.views.index'),
 	url(r'^trip/', include('trip.urls')),
-
 )
